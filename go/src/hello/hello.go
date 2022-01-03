45329ea3072de0a0b088ce9cdacf7d6cdbdcc970
colombohenrique <jhbcolombo@gmail.com>
2022-01-03 20:50:04 -0300

    Primeiros capitulos curso GO

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!!  balbla")
+	nome := "José"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi: ", comando)
+
 }
