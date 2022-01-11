a00528dae0ba78292c48d28b342895623fb2b013
colombohenrique <jhbcolombo@gmail.com>
2022-01-10 21:56:55 -0300

    Finalização curso Orientacao a Objetos em Go, alura, capitulo 5

--- go/src/banco/main.go
+++ go/src/banco/main.go
@@ -6,6 +6,14 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	clienteBruno := clientes.Titular{
 		Nome:      "Bruno",
@@ -17,8 +25,16 @@ func main() {
 		Titular:       clienteBruno,
 		NumeroAgencia: 123,
 		NumeroConta:   123456,
-		Saldo:         100,
 	}
 
-	fmt.Println(contaBruno)
+	contaBruno.Depositar(100)
+	PagarBoleto(&contaBruno, 55)
+
+	fmt.Println(contaBruno.ObterSaldo())
+
+	contaDenis := contas.ContaPoupanca{}
+	contaDenis.Depositar(100)
+	PagarBoleto(&contaDenis, 60)
+
+	fmt.Println(contaDenis.ObterSaldo())
 }
