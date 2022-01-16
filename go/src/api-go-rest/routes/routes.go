621e95f0bd63fa6559ad3faf40e455065dadb7b3
colombohenrique <jhbcolombo@gmail.com>
2022-01-16 18:34:50 -0300

    Atualizando ContentType e adicionando Middleware

--- go/src/api-go-rest/routes/routes.go
+++ go/src/api-go-rest/routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/Alura/go/src/go-rest-api/controllers"
+	"github.com/Alura/go/src/go-rest-api/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
