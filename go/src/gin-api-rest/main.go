d7b8f26d900a223228e961adb0d41f2677395f85
colombohenrique <jhbcolombo@gmail.com>
2022-03-14 19:17:34 -0300

    Atualizando configurações de Docker e DataBase

--- go/src/gin-api-rest/main.go
+++ go/src/gin-api-rest/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"gin-api-rest/database"
 	"gin-api-rest/models"
 	"gin-api-rest/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "José", CPF: "11111111111", RG: "111111111"},
 		{Nome: "João", CPF: "11111111111", RG: "111111111"},
