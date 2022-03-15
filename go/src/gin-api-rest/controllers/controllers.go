9a0c7768a6c359d8d8d01bd15da84e96e7ea0263
colombohenrique <jhbcolombo@gmail.com>
2022-03-15 14:03:41 -0300

    Atualizando routes e controllers

--- go/src/gin-api-rest/controllers/controllers.go
+++ go/src/gin-api-rest/controllers/controllers.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"gin-api-rest/database"
 	"gin-api-rest/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,4 +17,16 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz: ": "E ai " + nome + ", tudo beleza?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
