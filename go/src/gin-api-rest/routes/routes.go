cfb93619ea2cbbb23851c99c3f1dcef2ec200f53
colombohenrique <jhbcolombo@gmail.com>
2022-03-17 16:14:27 -0300

    Finalização da API e conclusão do curso

--- go/src/gin-api-rest/routes/routes.go
+++ go/src/gin-api-rest/routes/routes.go
@@ -11,5 +11,9 @@ func HadnleRequest() {
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/:nome", controllers.Saudacao)
 	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run()
 }
